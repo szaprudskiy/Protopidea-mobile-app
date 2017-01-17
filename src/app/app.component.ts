9260f4406ecc79cf4329107c7e8ce765a48bf43e
MadOtaku89 <zvezdo4et89@gmail.com>
2017-01-17 14:18:07 +0200

    adding splash screens and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ import { NavController } from 'ionic-angular';
 export class MyApp {
   @ViewChild('myNav') nav: NavController
 
-  rootPage: any = RegPage;
+  rootPage: any = IdeaCreatePage;
 
 
   constructor(
@@ -38,7 +38,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      Splashscreen.hide();
+      //Splashscreen.hide();
     });
   
 
